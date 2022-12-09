2bb8a165f9bce7dc1f99d2bbe218a6e2968bd688
Anthony Twesigye <twesigye10@gmail.com>
2022-12-09 12:10:28 +0300

    analysis with subsets for both main and roster

--- R/analysis_script_pa.R
+++ R/analysis_script_pa.R
@@ -8,9 +8,17 @@ data_nms <- names(readxl::read_excel(path = data_path, n_max = 2000))
 c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "guess")
 
 df_ref_with_weights <- readxl::read_excel(path = data_path, sheet = "UGA2207_PA", col_types = c_types)
+df_roster_with_weights <- readxl::read_excel(path = data_path, sheet = "hh_roster", col_types = c_types)
 
 # dap
 dap <- read_csv("inputs/r_dap_uga_pa.csv")
 
 # set up design objects
 ref_svy <- as_survey(.data = df_ref_with_weights, strata = strata, weights = weights )
+
+df_main_analysis <- analysis_support_after_survey_creation(input_ref_svy = ref_svy,
+                                                           input_dap = dap %>% filter(!variable %in% c("gender", "age", "i.age")))
+
+df_roster_analysis <- analysis_support_after_survey_creation(input_ref_svy = df_roster_with_weights,
+                                                           input_dap = dap %>% filter(variable %in% c("gender", "age", "i.age")))
+
