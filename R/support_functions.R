eb86064f4f9299b1b6c2fbf97abd2d13667a75fc
Anthony Twesigye <twesigye10@gmail.com>
2022-12-10 11:32:37 +0300

    added some functions for extra data cleaning

--- R/support_functions.R
+++ R/support_functions.R
@@ -282,7 +282,7 @@ check_duplicates_by_uuid <- function(input_tool_data) {
     filter(rank > 1) %>%  
     mutate(
       i.check.type = "remove_survey",
-      i.check.name = "hh_id",
+      i.check.name = "household_id",
       i.check.current_value = "",
       i.check.value = "",
       i.check.issue_id = "duplicate_uuid",
@@ -295,6 +295,54 @@ check_duplicates_by_uuid <- function(input_tool_data) {
       i.check.adjust_log = "",
       i.check.uuid_cl = "",
       i.check.so_sm_choices = "")%>% 
+    dplyr::select(starts_with("i.check"))%>% 
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
+
+# check for duplicate hhid numbers
+check_duplicate_hhid_numbers <- function(input_tool_data, input_sample_hhid_nos_list) {
+  input_tool_data %>% 
+    mutate(unique_hhid_number = household_id) %>% 
+    group_by(i.check.location, i.check.household_id) %>% 
+    filter(n() > 1, unique_hhid_number %in% input_sample_hhid_nos_list) %>% 
+    mutate(i.check.type = "change_response",
+           i.check.name = "household_id",
+           i.check.current_value = household_id,
+           i.check.value = "",
+           i.check.issue_id = "hhid_c_duplicate_hhid_no",
+           i.check.issue = glue("household_id: {household_id} is duplicated: check that its not a repeated survey"),
+           i.check.other_text = "",
+           i.check.checked_by = "",
+           i.check.checked_date = as_date(today()),
+           i.check.comment = "", 
+           i.check.reviewed = "",
+           i.check.adjust_log = "",
+           i.check.uuid_cl = "",
+           i.check.so_sm_choices = "") %>% 
+    ungroup() %>%
+    dplyr::select(starts_with("i.check"))%>% 
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
+
+# check for point number not being in samples
+check_hhid_number_not_in_samples <- function(input_tool_data, input_sample_hhid_nos_list) {
+  input_tool_data %>% 
+    mutate(unique_hhid_number = household_id) %>% 
+    filter(!unique_hhid_number %in% input_sample_hhid_nos_list) %>% 
+    mutate(i.check.type = "change_response",
+           i.check.name = "household_id",
+           i.check.current_value = household_id,
+           i.check.value = "",
+           i.check.issue_id = "hhid_c_hhid_no_not_in_sample",
+           i.check.issue = glue("household_id: {household_id} not in samples"),
+           i.check.other_text = "",
+           i.check.checked_by = "",
+           i.check.checked_date = as_date(today()),
+           i.check.comment = "", 
+           i.check.reviewed = "",
+           i.check.adjust_log = "",
+           i.check.uuid_cl = "",
+           i.check.so_sm_choices = "") %>% 
     dplyr::select(starts_with("i.check"))%>% 
     rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 }
