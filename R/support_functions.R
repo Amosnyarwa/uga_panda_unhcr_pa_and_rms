fffb00345da60a50f62a6bcb1e083ac78de1bf6d
Anthony Twesigye <twesigye10@gmail.com>
2022-12-10 11:07:58 +0300

    uplicate uuids

--- R/support_functions.R
+++ R/support_functions.R
@@ -270,3 +270,31 @@ analysis_support_after_survey_creation <- function(input_ref_svy, input_dap) {
   
   bind_rows(outputs)
 }
+
+
+
+# duplicate uuids: these were phone calls ---------------------------------
+
+check_duplicates_by_uuid <- function(input_tool_data) {
+  input_tool_data %>% 
+    group_by(i.check.uuid) %>% 
+    mutate(rank = row_number()) %>% 
+    filter(rank > 1) %>%  
+    mutate(
+      i.check.type = "remove_survey",
+      i.check.name = "hh_id",
+      i.check.current_value = "",
+      i.check.value = "",
+      i.check.issue_id = "duplicate_uuid",
+      i.check.issue = "The uuid: {i.check.uuid} is duplicate in the data",
+      i.check.other_text = "",
+      i.check.checked_by = "",
+      i.check.checked_date = as_date(today()),
+      i.check.comment = "", 
+      i.check.reviewed = "",
+      i.check.adjust_log = "",
+      i.check.uuid_cl = "",
+      i.check.so_sm_choices = "")%>% 
+    dplyr::select(starts_with("i.check"))%>% 
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
