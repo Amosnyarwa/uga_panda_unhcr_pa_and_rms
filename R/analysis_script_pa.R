069874ec9e2a3a22a2b91484a7d95ae07b2f4a34
Anthony Twesigye <twesigye10@gmail.com>
2022-12-10 09:58:26 +0300

    adding tool and combining analysis

--- R/analysis_script_pa.R
+++ R/analysis_script_pa.R
@@ -10,6 +10,9 @@ c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "g
 df_ref_with_weights <- readxl::read_excel(path = data_path, sheet = "UGA2207_PA", col_types = c_types)
 df_roster_with_weights <- readxl::read_excel(path = data_path, sheet = "hh_roster")
 
+# tool
+df_survey <- readxl::read_excel("inputs/participatory_assessment_tool.xlsx", sheet = "survey")
+
 # dap
 dap <- read_csv("inputs/r_dap_uga_pa.csv")
 
@@ -17,8 +20,17 @@ dap <- read_csv("inputs/r_dap_uga_pa.csv")
 ref_svy <- as_survey(.data = df_ref_with_weights, strata = strata, weights = weights )
 
 df_main_analysis <- analysis_support_after_survey_creation(input_ref_svy = ref_svy,
-                                                           input_dap = dap %>% filter(!variable %in% c("gender", "age", "i.age")))
+                                                           input_dap = dap |> filter(!variable %in% c("gender", "age", "i.age")))
 
 df_roster_analysis <- analysis_support_after_survey_creation(input_ref_svy = df_roster_with_weights,
-                                                           input_dap = dap %>% filter(variable %in% c("gender", "age", "i.age")))
+                                                           input_dap = dap |> filter(variable %in% c("gender", "age", "i.age")))
 
+# merge analysis ----------------------------------------------------------
+
+combined_analysis <- bind_rows(df_main_analysis, df_roster_analysis)
+ 
+full_analysis_long <- combined_analysis |> 
+  
+
+full_analysis_long |>
+  write_csv(paste0("outputs/", butteR::date_file_prefix(), "_full_analysis_lf_caregiver.csv"), na="")
