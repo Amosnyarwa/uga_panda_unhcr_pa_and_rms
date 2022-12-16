8700d83f43d3a60b7e8016913b50d19bc48fb9c7
Anthony Twesigye <twesigye10@gmail.com>
2022-12-16 13:35:06 +0300

    removed weights

--- R/composite_indicators_and_weights.R
+++ R/composite_indicators_and_weights.R
@@ -32,25 +32,3 @@ create_composite_indicators_pa <- function(input_df) {
     ) |> 
     select(-c(starts_with("int.")))
 }
-
-
-# refugee weights ---------------------------------------------------------
-
-make_refugee_weight_table<- function(input_df_ref, input_refugee_pop){
-  refugee_pop <- input_refugee_pop %>% 
-    mutate(strata= paste0(strata, "_refugee"),
-           pop_status= "refugee")
-  
-  refugee_weight_table <- input_df_ref %>% 
-    group_by(strata) %>% 
-    summarise(
-      sample_strata=  n()
-    ) %>% 
-    mutate(
-      sample_global= sum(sample_strata)) %>% 
-    left_join(refugee_pop) %>% 
-    rename(pop_strata= "total_pop") %>% 
-    mutate(pop_global=sum(pop_strata) ,
-           weights = (pop_strata/pop_global)/(sample_strata/sample_global)
-    )
-}
