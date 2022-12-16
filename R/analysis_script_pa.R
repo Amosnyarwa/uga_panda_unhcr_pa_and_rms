dd2784a6fb7971652496d9a6fb34cf9f41beb93e
Anthony Twesigye <twesigye10@gmail.com>
2022-12-16 13:40:27 +0300

    renamed datasets and removed weighting from the analysis

--- R/analysis_script_pa.R
+++ R/analysis_script_pa.R
@@ -7,8 +7,8 @@ data_path <- "inputs/clean_data_unhcr_pa.xlsx"
 data_nms <- names(readxl::read_excel(path = data_path, n_max = 2000))
 c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "guess")
 
-df_ref_with_weights <- readxl::read_excel(path = data_path, sheet = "UGA2207_PA", col_types = c_types)
-df_roster_with_weights <- readxl::read_excel(path = data_path, sheet = "hh_roster")
+df_main_clean_data <- readxl::read_excel(path = data_path, sheet = "UGA2207_PA", col_types = c_types)
+df_roster_clean_data <- readxl::read_excel(path = data_path, sheet = "hh_roster")
 
 # tool
 df_survey <- readxl::read_excel("inputs/participatory_assessment_tool.xlsx", sheet = "survey") 
@@ -22,12 +22,12 @@ df_tool_data_support <- df_survey |>
 dap <- read_csv("inputs/r_dap_uga_pa.csv")
 
 # set up design objects
-ref_svy <- as_survey(.data = df_ref_with_weights, strata = strata, weights = weights )
+ref_svy <- as_survey(.data = df_main_clean_data)
 
 df_main_analysis <- analysis_support_after_survey_creation(input_ref_svy = ref_svy,
                                                            input_dap = dap |> filter(!variable %in% c("gender", "age", "i.age")))
 
-df_roster_analysis <- analysis_support_after_survey_creation(input_ref_svy = df_roster_with_weights,
+df_roster_analysis <- analysis_support_after_survey_creation(input_ref_svy = df_roster_clean_data,
                                                            input_dap = dap |> filter(variable %in% c("gender", "age", "i.age")))
 
 # merge analysis ----------------------------------------------------------
