00b84770875fe3bae0debd06023f83be41c5f66e
Anthony Twesigye <twesigye10@gmail.com>
2023-01-19 14:34:55 +0300

    updated formating of analysis with question groups or sectors

--- R/pa_analysis_script.R
+++ R/pa_analysis_script.R
@@ -22,6 +22,8 @@ df_tool_data_support <- df_survey |>
 dap <- read_csv("inputs/r_dap_uga_pa.csv") |> 
   filter(!str_detect(string = subset_1, pattern = " |household_type|gender"))
 
+df_question_type_data <- read_csv("inputs/r_question_groups_pa.csv")
+
 # set up design objects
 ref_svy <- as_survey(.data = df_main_clean_data)
 
@@ -37,14 +39,16 @@ df_roster_analysis <- analysis_support_after_survey_creation(input_ref_svy = df_
 combined_analysis <- df_main_analysis
  
 full_analysis_long <- combined_analysis |> 
-  mutate(variable = ifelse(is.na(variable), variable_val, variable),
+  mutate(variable = ifelse(is.na(variable) | variable %in% c(""), variable_val, variable),
          int.variable = ifelse(str_detect(string = variable, pattern = "^i\\."), str_replace(string = variable, pattern = "^i\\.", replacement = ""), variable)) %>% 
   left_join(df_tool_data_support, by = c("int.variable" = "name")) %>% 
   relocate(label, .after = variable) %>% 
   mutate(label = ifelse(is.na(label), variable, label),
-         `mean/pct` = ifelse(select_type %in% c("integer") & !str_detect(string = variable, pattern = "^i."), `mean/pct`, `mean/pct`*100),
+         `mean/pct` = ifelse(select_type %in% c("integer") & !str_detect(string = variable, pattern = "^i\\."), `mean/pct`, `mean/pct`*100),
          `mean/pct` = round(`mean/pct`, digits = 2)) %>%
-  select(`Question`= label, variable, `choices/options` = variable_val, `Results(mean/percentage)` = `mean/pct`, n_unweighted, population, subset_1_name, subset_1_val)
+  left_join(df_question_type_data, by = c("int.variable" = "kobo_question")) |> 
+  mutate(indicator_group_sector = ifelse(variable == "i.disability", "Demographics", indicator_group_sector)) |> 
+  select(`Question`= label, `Question Group/Sector` = indicator_group_sector, variable, `choices/options` = variable_val, `Results(mean/percentage)` = `mean/pct`, n_unweighted, population, subset_1_name, subset_1_val)
 
 write_csv(full_analysis_long, paste0("outputs/", butteR::date_file_prefix(), "_full_analysis_lf_pa.csv"), na="")
 write_csv(full_analysis_long, paste0("outputs/full_analysis_lf_pa.csv"), na="")
