2a401f5563b80860e267403d19fe934c0df43c46
Anthony Twesigye <twesigye10@gmail.com>
2023-02-07 12:33:29 +0300

    i.REF12

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -42,7 +42,20 @@ create_composite_indicators_rms <- function(input_df) {
                                    respondent_age > 59 ~ "age_greater_59",
                                    TRUE ~ "NA"),
       i.UNI01 = case_when(UNI01 > 0 ~ "Yes",
-                          TRUE ~ "No")
+                          TRUE ~ "No"),
+      int.REF12a = case_when(str_detect(string = REF12a, pattern = "-") & !str_detect(string = REF12a, pattern = "9998|9999") ~ str_extract(string = my(REF12a), pattern = "^[0-9]{4}"), 
+                             str_detect(string = REF12a, pattern = "/") & !str_detect(string = REF12a, pattern = "9998|9999") ~ str_extract(string = REF12a, pattern = "[0-9]{4}$")
+      ),
+      int.REF12b = case_when(str_detect(string = REF12b, pattern = "-") & !str_detect(string = REF12b, pattern = "9998|9999") ~ str_extract(string = my(REF12b), pattern = "^[0-9]{4}"), 
+                             str_detect(string = REF12b, pattern = "/") & !str_detect(string = REF12b, pattern = "9998|9999") ~ str_extract(string = REF12b, pattern = "[0-9]{4}$")
+      ),
+      int.REF12 = ifelse(is.na(int.REF12a) & !is.na(int.REF12b), int.REF12b, int.REF12a),
+      int.year_arrival_interval = interval(as_date(paste0(int.REF12, "0101")), as_date(today)),
+      int.length_since_year_arrival = time_length(int.year_arrival_interval, "year"),
+      i.REF12 = case_when(int.length_since_year_arrival <= 1 ~ "within_1_yr_ago",
+                          int.length_since_year_arrival <= 5 ~ "1_and_5_yrs_ago",
+                          int.length_since_year_arrival <= 10 ~ "5_and_10_yrs_ago",
+                          int.length_since_year_arrival > 10 ~ "greater_10_yrs_ago")
     ) |> 
     select(-c(starts_with("int.")))
 }
